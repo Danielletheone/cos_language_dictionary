35e3224ad5184160faa3a5e78d787918a7385c25
Danielle <danielleeechoi@gmail.com>
2025-01-18 14:49:28 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+dictionary = {
+        "Yoruba":  {"wa": "come", "iwo": "you", "oruko": "name",  "omo": "child", "ile": "house",
+    "oloye": "king",  "agbara": "strength", "omolode": "beautiful",  "eniyan": "person", "oko": "husband",
+    "iyawo": "wife", "ounje": "food","omi": "water", "orun": "heaven", "aye": "earth", "okan": "heart", "owo": "hand",
+    "oko": "farm", "ojo": "day", "orun": "sleep"
+}
