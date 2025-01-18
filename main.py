28c6cf540b88c0ce703ec44c8a7deac9e8081f48
Danielle <danielleeechoi@gmail.com>
2025-01-18 23:33:56 +0100

    i added the tkinter window and translation function

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from tkinter import *
+from tkinter import messagebox
+
+def translate(languages,entry):
+    word = entry.get()
+    lang = language.get()
+    translation = dictionary.get(lang, {}).get(word)
+    messagebox.showinfo("Result" , f"{word.capitalize()} : {translation}")
 dictionary = {
         "Yoruba":  {"wa": "come", "iwo": "you", "oruko": "name",  "omo": "child", "ile": "house",
     "oloye": "king",  "agbara": "strength", "omolode": "beautiful",  "eniyan": "person", "oko": "husband",
@@ -5,3 +13,27 @@ dictionary = {
     "oko": "farm", "ojo": "day", "orun": "sleep"
 }
 }
+
+window = Tk()
+window.title("Language Dictionary")
+
+languages = ["Yoruba"]
+language = StringVar(window)
+language.set("Pick a Language")
+language_menu = OptionMenu(window, language, *languages)
+language_menu.pack()
+
+words_label = Label(window, text="Enter Word:")
+words_label.pack()
+
+entry = Entry(window)
+entry.pack(pady=10)
+
+translate_button = Button(window, text="Translate",  command=lambda: translate(languages, entry))
+translate_button.pack()
+
+result_label = Label(window, text="")
+result_label.pack()
+
+window.mainloop()
+
